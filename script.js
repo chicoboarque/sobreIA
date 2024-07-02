64c34291a1ae2fc6d15167a4cae6af3a3b2c14fd
chicoboarque <161610158+chicoboarque@users.noreply.github.com>
2024-07-02 17:44:11 -0300

    0207

--- script.js
+++ script.js
@@ -29,13 +29,16 @@ const perguntas = [
 let atual = 0;
 let perguntaAtual;
 
-function mostraPergunta() {
+function mostraPergunta() 
+{
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
 }
-function mostraAlternativas() {
-    for(const alternativa of perguntaAtual.alternativas) {
+function mostraAlternativas() 
+{
+    for(const alternativa of perguntaAtual.alternativas) 
+    {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa;
         caixaAlternativas.appendChild(botaoAlternativas);
